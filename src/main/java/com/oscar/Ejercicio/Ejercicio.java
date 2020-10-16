58db6d176cc93a48a47d016e34b44dc78cfb4562
oscar <oscarGradoClase@gmail.com>
2020-10-16 12:37:26 +0200

    subida

---
+++ src/main/java/com/oscar/Ejercicio/Ejercicio.java
@@ -0,0 +1,52 @@
+
+package com.oscar.Ejercicio;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.Random;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Ejercicio
+ */
+
+@WebServlet("/Ejercicio")
+public class Ejercicio extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Ejercicio() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		out.print("<html><body>");
+		out.print("<h1>Esta es la fecha: " + new Date());
+		out.print("</h1></body></html>");
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+	<body>
+		<h2>¿Quieres saber la fecha maquina?</h2>
+		<form action="http://localhost:8080/TrabajoMaven/Ejercicio" method="post">
+			<input type="submit" name="enviar">
+		</form>
+	
+	</body>
+</html>
